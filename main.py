e7923f36b16808b934c426f1ae717d93765c74ba
Çağrı KİBAR <77233335+CagriKibar@users.noreply.github.com>
2021-05-16 01:57:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import pyautogui
+import time
+
+pyautogui.rightClick(1706, 508)
+pyautogui.leftClick(1706, 508)
+time.sleep(3)
+for i in range(2000000):
+    for j in range(2):
+        pyautogui.rightClick(1575, 510)
+        pyautogui.rightClick(1976, 709)
+        time.sleep(0.75)
+        pyautogui.rightClick(2006, 713)
+        pyautogui.rightClick(1975, 709)
+        pyautogui.rightClick(1976, 709)
+        pyautogui.rightClick(2036, 715)
+
+
+        time.sleep(0.75)
+        pyautogui.rightClick(2006, 713)
+        pyautogui.rightClick(1975, 709)
+        pyautogui.rightClick(1976, 709)
+        pyautogui.rightClick(2036, 715)
+
+
+        time.sleep(0.75)
+        pyautogui.rightClick(1975, 709)
+        pyautogui.rightClick(1976, 709)
+
+        time.sleep(1.8)
+        pyautogui.rightClick(2175, 712)
+        pyautogui.rightClick(2175, 712)
+        pyautogui.rightClick(1975, 709)
+        pyautogui.rightClick(1976, 709)
+
+
